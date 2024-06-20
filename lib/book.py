60e60b5ec2ffd3c4c51ae79f449b02fcb34ed202
JosephMainaOtundo <otundomainajoseph@gmail.com>
2024-06-20 09:55:44 +0300

    7 failing

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+
+    def get_page_count(self):
+        try:
+            return self._page_count
+        except:
+            return
     
-        
+    def set_page_count(self, page_count):
+        if (type(page_count) == int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+    page_count = property(get_page_count, set_page_count)
+
+# Book in book.py gets initialized with a title.
+# Book in book.py has the title passed into __init__.
+# Book in book.py can be assigned an author name.
+# Book in book.py can be assigned a page count property.
+# Book in book.py prints "page_count must be an integer" if page_count is not an integer.
+# Book in book.py can be assigned a genre.
+# Book in book.py outputs "Flipping the page...wow, you read fast!" when method turn_page() is called
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand):
+        self.brand = brand
+
+    
+
+    def get_size(self):
+        try:
+            return self._size
+        except:
+            return
+    
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+# Shoe in shoe.py gets initialized with a brand.
+# Shoe in shoe.py has the brand passed to __init__.
+# Shoe in shoe.py can be assigned a color.
+# Shoe in shoe.py can be assigned a size.
+# Shoe in shoe.py prints "size must be an integer" if size is not an integer.
+# Shoe in shoe.py can be assigned a material.
+# Shoe in shoe.py can be assigned a condition.
+# Shoe in shoe.py says that the shoe has been repaired.
+# Shoe in shoe.py has "New" condition after repair.
